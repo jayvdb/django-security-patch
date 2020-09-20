bc2803edc91d2459ccff67b33d87257a35113219
John Vandenberg <jayvdb@gmail.com>
2020-09-20 16:42:08 +0700

    Apply to path and POST

--- django_security_patch/middleware.py
+++ django_security_patch/middleware.py
@@ -5,6 +5,9 @@ def QueryStringsSanitizer(get_response):
 
     def wrapper(request):
 
+        for rm in settings.QUERY_REMOVE_STRINGS:
+            request.path = request.path.replace(rm, '')
+
         if True:
             # Only apply to get requests
             query_strings = []
@@ -31,8 +34,33 @@ def QueryStringsSanitizer(get_response):
                 edit_get[query_strings[i][0]] = query_strings[i][1]
 
             request.GET = edit_get
-            response = get_response(request)
-        else:
+
+            # Only apply to get requests
+            query_strings = []
+
+            for (key, value) in dict(request.POST.lists()).items():
+
+                try:
+                    # replace not allowed strings with '' for each querystring value.
+                    for rm in settings.QUERY_REMOVE_STRINGS:
+                        # Value is it list for example:
+                        # ?name=test   with be received here in format: {'name': ['test']}
+                        value[0] = value[0].replace(rm, '')
+                except:
+                    # Ignore if any error happened
+                    # TODO: Check situations that lead to here, may cause some leaks!
+                    continue
+
+                # Now cleaned data push to final list
+                query_strings.append((key, value[0]))
+
+            edit_get = request.POST.copy()
+            # Get a copy of request.GET and update filtered values to new ones.
+            for i in range(len(query_strings)):
+                edit_get[query_strings[i][0]] = query_strings[i][1]
+
+            request.POST = edit_get
+
             response = get_response(request)
 
         return response
